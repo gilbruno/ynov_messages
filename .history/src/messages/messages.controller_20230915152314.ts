6929e870134d1a95087b9c25616a1d3f37b32171
Gilles Bruno <gilles_bruno@gmx.fr>
2023-09-15 15:24:29 +0200

    Init commit

---
+++ .history/src/messages/messages.controller_20230915152314.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+
+    @Get('/:id')
+    getMsg(id: number) {
+        
+    }
+
+    @Get()
+    listMessages() {
+
+    }
+
+    @Post()
+    createMessage() {
+
+    }
+
+
+}
